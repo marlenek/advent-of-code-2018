e6bc540ce1a7a41a0a3364bea9b5e53264c317f7
Marlena <ma.zdybel@gmail.com>
2018-12-02 12:26:04 +0100

    changes variables names

--- day-2/src/main/scala/Checksum.scala
+++ day-2/src/main/scala/Checksum.scala
@@ -4,9 +4,9 @@ import scala.io.Source
 
 object Checksum {
 
-  def part1(boxIds: List[String]): Int = {
+  def part1(boxesList: List[String]): Int = {
     var boxesWithTwoLetters, boxesWithThreeLetters = 0
-    boxIds.foreach { b =>
+    boxesList.foreach { b =>
       val boxMap = b.groupBy(_.toChar).mapValues(_.size)
       if (boxMap.exists(_._2 == 2)) boxesWithTwoLetters += 1
       if (boxMap.exists(_._2 == 3)) boxesWithThreeLetters += 1
@@ -14,21 +14,19 @@ object Checksum {
     boxesWithTwoLetters * boxesWithThreeLetters
   }
 
-  def part2(boxIds: List[String]): String = {
-    val nrOfBoxes = boxIds.length
-    val lengthOfBox = boxIds.head.size
-
-    for (i <- 0 to lengthOfBox - 1) {
-      val newBoxes = boxIds.map(s => s.substring(0, i).concat(s.substring(i + 1, lengthOfBox)))
-      if (newBoxes.toSet.size != nrOfBoxes)
-        return newBoxes.diff(newBoxes.distinct).head
+  def part2(boxesList: List[String]): String = {
+    val boxLength = boxesList.head.size
+    for (i <- 0 to boxLength - 1) {
+      val boxesWithoutOneLetter = boxesList.map(s => s.substring(0, i).concat(s.substring(i + 1, boxLength)))
+      if (boxesWithoutOneLetter.toSet.size != boxesList.length)
+        return boxesWithoutOneLetter.diff(boxesWithoutOneLetter.distinct).head
     }
     ""
   }
 
   def main(args: Array[String]): Unit = {
-    val boxIds = Source.fromResource("input.txt").getLines().toList
-    println("Result for part 1: " + part1(boxIds))
-    println("Result for part 2: " + part2(boxIds))
+    val boxesList = Source.fromResource("input.txt").getLines().toList
+    println("Result for part 1: " + part1(boxesList))
+    println("Result for part 2: " + part2(boxesList))
   }
 }
