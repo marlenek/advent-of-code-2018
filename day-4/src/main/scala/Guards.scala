5af57358b20d4efb2d5fdb34bf8256baed8da908
Marlena <ma.zdybel@gmail.com>
2018-12-05 23:23:24 +0100

    day 4 fixed

--- day-4/src/main/scala/Guards.scala
+++ day-4/src/main/scala/Guards.scala
@@ -18,7 +18,7 @@ object Guards {
 
   case class Guard(time: DateTime, id: Int, activity: Activity)
 
-  case class GuardActivity(id: Int, timeSpent: List[Int], activity: Activity)
+  case class GuardActivity(id: Int, minutes: List[Int], activity: Activity)
 
   def matchGuard(line: String): Guard = {
     val formatter: DateTimeFormatter = DateTimeFormat.forPattern("yyyy-MM-dd HH:mm")
@@ -34,43 +34,40 @@ object Guards {
     }
   }
 
-  def main(args: Array[String]): Unit = {
-    val guards = Source.fromResource("input.txt").getLines().toList.map(line => matchGuard(line))
-    //  println(guards)
-
-    val guards2 = guards.sortBy(_.time.getMillis)
-    println(guards2.size)
-
+  def sortGuardsByActivity(guards: List[Guard]): List[GuardActivity] = {
+    val guardsSorted = guards.sortBy(_.time.getMillis)
     val guardsActivity = new ListBuffer[GuardActivity]()
-
     var id = -1
-    for (i <- 0 until guards2.length - 1) {
-      val curr = guards2(i)
-      val next = guards2(i + 1)
+    for (i <- 0 until guardsSorted.length - 1) {
+      val curr = guardsSorted(i)
+      val next = guardsSorted(i + 1)
       if (curr.id != -1)
         id = curr.id
-
-      curr.activity match {
-        case Awake => guardsActivity += GuardActivity(id, (curr.time.getMinuteOfHour to next.time.getMinuteOfHour - 1).toArray.toList, Awake)
-        case Asleep => guardsActivity += GuardActivity(id, (curr.time.getMinuteOfHour to next.time.getMinuteOfHour - 1).toArray.toList, Asleep)
-      }
+      guardsActivity += GuardActivity(id, (curr.time.getMinuteOfHour to next.time.getMinuteOfHour - 1).toArray.toList, curr.activity)
     }
+    guardsActivity.toList
+  }
 
-    println(guardsActivity.size)
-    var sleepyhead = guardsActivity.toList.filter(_.activity == Asleep).groupBy(_.id).mapValues(_.flatMap(_.timeSpent).size).maxBy(_._2)._1
-    var sleepyMinute = guardsActivity.toList.filter(_.activity == Asleep).groupBy(_.id).mapValues(_.flatMap(_.timeSpent)).filter(_._1 == sleepyhead).values.flatten.groupBy(identity).maxBy(_._2.size)._1
+  def getBestGuardAndMinute(guardsActivity: List[GuardActivity]): Int = {
 
-    var sleepyHead3 = guardsActivity.toList.filter(_.activity == Asleep).groupBy(_.id).mapValues(_.flatMap(_.timeSpent))
-      .mapValues(_.groupBy(identity).mapValues(_.size)).maxBy(_._2.maxBy(_._2))
+    val sleepyGuards = guardsActivity.filter(_.activity == Asleep).groupBy(_.id)
+    var sleepyHead = sleepyGuards.mapValues(_.flatMap(_.minutes).size).maxBy(_._2)._1
+    var sleepyMinute = sleepyGuards.mapValues(_.flatMap(_.minutes)).filter(_._1 == sleepyHead).values.flatten.groupBy(identity).maxBy(_._2.size)._1
 
-    var sleepyHead2 = guardsActivity.toList.filter(_.activity == Asleep).groupBy(_.id).mapValues(_.flatMap(_.timeSpent))
-      .mapValues(_.groupBy(identity).mapValues(_.size)).maxBy(_._2.maxBy(_._2))._2.maxBy(_._2)._1 //.groupBy(_._2.identity).mapValues(_.size)
-    // list.groupBy(identity).mapValues(_.size)
-    println(sleepyHead2 + " " + sleepyHead3 + " " )//+ (sleepyHead2 * sleepyHead3))
-   // println(sleepyHead2 * sleepyHead3)
+    sleepyHead * sleepyMinute
+  }
 
+  def getMostSleepyMinute(guardsActivity: List[GuardActivity]): Int = {
 
+    var sleepyMinuteGuard = guardsActivity.filter(_.activity == Asleep).groupBy(_.id).mapValues(_.flatMap(_.minutes))
+      .mapValues(_.groupBy(identity).mapValues(_.size)).mapValues(_.maxBy(_._2)).maxBy(_._2._2)
+
+    sleepyMinuteGuard._1 * sleepyMinuteGuard._2._1
+  }
+
+  def main(args: Array[String]): Unit = {
+    val guards = sortGuardsByActivity(Source.fromResource("input.txt").getLines().toList.map(line => matchGuard(line)))
+    println("Part 1: " + getBestGuardAndMinute(guards))
+    println("Part 2: " + getMostSleepyMinute(guards))
   }
 }
-
-//112372 zle
